b0ee99f4cd47bf8ae6ce1812a2e96adc502504ca
Peter Zhang <peter.zhang.henan@gmail.com>
2018-10-12 18:14:41 +0800

    button

--- views/button.vue
+++ views/button.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="page">
+    <div class="">
         <div class="page__hd">
             <h1>Button</h1>
         </div>
         <div class="page__bd">
             <z-button color="primary" lg>button lg</z-button>
-            <z-button color="primary" block>button normal</z-button>
+            <z-button color="default" block outline>button normal</z-button>
             <z-button color="primary" md>button md</z-button>
             <z-button color="primary" xs>button xs</z-button>
             <z-button color="primary" sm>button sm</z-button>
