a4ac3b183098a4b8480a81842bdb7af42559c507
Peter Zhang <peter.zhang.henan@gmail.com>
2018-10-23 14:06:54 +0800

    scroll

--- components/scroll/App.vue
+++ components/scroll/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-infinite-scroll="onScroll" infinite-scroll-disabled="loading" class="zg-scroll" infinite-scroll-immediate-check="true">
-        <template v-for="(item, index) of data" :key="index">
+        <template v-for="(item, index) of data">
             <slot v-bind:item="item"></slot>
         </template>
     </div>
