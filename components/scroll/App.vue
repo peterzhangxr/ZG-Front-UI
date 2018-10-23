a280504adb613f4c63c0be1df9a97c759596a691
Peter Zhang <peter.zhang.henan@gmail.com>
2018-10-23 14:41:08 +0800

    fix scroll bug

--- components/scroll/App.vue
+++ components/scroll/App.vue
@@ -3,10 +3,15 @@
         <template v-for="(item, index) of data">
             <slot v-bind:item="item"></slot>
         </template>
+        <div v-if="loading && !loaded" class="zg-scroll__spinner">
+            <Spinner :size="size" color="light"></Spinner>
+            <div class="zg-scroll__spinner--text">{{ text }}</div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Spinner from '../spinner'
     export default {
         props: {
             data: {
@@ -20,9 +25,32 @@
                 default: () => {}
             },
 
-            loading: Boolean,
+            loading: {
+                type: Boolean,
+                default: false
+            }
 
-            distance: 50
+            distance: {
+                type: Number,
+                default: 50
+            },
+            size: {
+                type: String,
+                default: '18px',
+            },
+            loaded: {
+                type: Boolean,
+                default: false,
+            },
+
+            text: {
+                type: String,
+                default: '努力加载...'
+            }
+
+        },
+        components: {
+            Spinner
         }
     }
 </script>
