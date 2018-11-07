15e0fb0950c70eb08138e0dccc32a6f6b0df0f5a
Peter Zhang <peter.zhang.henan@gmail.com>
2018-11-07 17:23:12 +0800

    tabbar

--- components/tabbar/App.vue
+++ components/tabbar/App.vue
@@ -4,26 +4,28 @@
             <slot></slot>
         </div>
         <div class="zg-tabbar" :class="{'zg-tabbar__fixed': fixed}">
-            <template v-for="(bar, index) of list">
-                <div v-if="active == index" class="zg-tabbar__item zg-tabbar__item__active">
-                    <div class="zg-tabbar__icon">
-                        <img :src="bar.iconSelected"/>
+            <slot>
+                <template v-for="(bar, index) of list">
+                    <div v-if="active == index" class="zg-tabbar__item zg-tabbar__item__active">
+                        <div class="zg-tabbar__icon">
+                            <img :src="bar.iconSelected"/>
+                        </div>
+                        <div class="zg-tabbar__text">{{ bar.title }}</div>
                     </div>
-                    <div class="zg-tabbar__text">{{ bar.title }}</div>
-                </div>
-                <div v-else-if="bar.redirect" @click="handleClick(bar)" class="zg-tabbar__item">
-                    <div class="zg-tabbar__icon">
-                        <img :src="bar.icon"/>
+                    <div v-else-if="bar.redirect" @click="handleClick(bar)" class="zg-tabbar__item">
+                        <div class="zg-tabbar__icon">
+                            <img :src="bar.icon"/>
+                        </div>
+                        <div class="zg-tabbar__text">{{ bar.title }}</div>
                     </div>
-                    <div class="zg-tabbar__text">{{ bar.title }}</div>
-                </div>
-                <router-link v-else :to="bar.to" class="zg-tabbar__item ">
-                    <div class="zg-tabbar__icon">
-                        <img :src="bar.icon"/>
-                    </div>
-                    <div class="zg-tabbar__text">{{ bar.title }}</div>
-                </router-link>
-            </template>
+                    <router-link v-else :to="bar.to" class="zg-tabbar__item ">
+                        <div class="zg-tabbar__icon">
+                            <img :src="bar.icon"/>
+                        </div>
+                        <div class="zg-tabbar__text">{{ bar.title }}</div>
+                    </router-link>
+                </template>
+            </slot>
         </div>
     </div>
 </template>
